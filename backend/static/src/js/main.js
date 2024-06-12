11689f8c360711e0256bb1e28a4652c5a1d4b099
jaman-bala <ermekoffdastan@yandex.ru>
2024-06-12 10:21:11 +0600

    up:16

--- backend/static/src/js/main.js
+++ backend/static/src/js/main.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
-    $('#bootstrapdatatable').DataTable({
-      "aLengthMenu": [[3, 5, 10, 25, -1], [3, 5, 10, 25, "All"]],
-        "iDisplayLength": 3
-       }
-    );
-} );
-console.log("main.js загружен");
-
-$(document).ready(function() {
+    // Проверка, инициализирована ли таблица DataTable
     if (!$.fn.DataTable.isDataTable('#bootstrapdatatable')) {
-        $('#bootstrapdatatable').DataTable();
+        $('#bootstrapdatatable').DataTable({
+            "aLengthMenu": [[3, 5, 10, 25, -1], [3, 5, 10, 25, "All"]],
+            "iDisplayLength": 3
+        });
     }
 });
+console.log("main.js загружен");
