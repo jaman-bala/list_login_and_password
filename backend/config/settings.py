5949c89374a80b6f0f500591130a1e36b84f85c1
jaman-bala <ermekoffdastan@yandex.ru>
2024-06-12 09:17:27 +0600

    up:6

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,17 +1,17 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://password.tsvs.kg']
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -108,13 +108,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# для сервера
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # MEDIA
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# для сервера
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+# MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
